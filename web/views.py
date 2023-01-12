443e894364c47425530b604e23fddf8cff880014
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 01:17:10 +0300

    + links

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from web.models import Note, Tag
 
