328a71101ab061154cc31661e40481ef726d2c62
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 02:41:21 +0300

    + forms - display in html

--- web/forms.py
+++ web/forms.py
@@ -1,19 +1,6 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
 
 class NoteForm(forms.Form):
-    title = forms.CharField(validators=[])
-    text = forms.CharField()
-
-    def clean_text(self):
-        raise ValidationError("текст неверный в принципе!")
-        return self.data['text'].strip()
-
-    def clean(self):
-        if 'title' in self.cleaned_data:
-            self.cleaned_data['title'] = self.cleaned_data['title'].upper()
-            return self.cleaned_data
-
-    #TODO clean
-    #TODO errors
+    title = forms.CharField(label='Название')
+    text = forms.CharField(widget=forms.Textarea(), label='Текст')
