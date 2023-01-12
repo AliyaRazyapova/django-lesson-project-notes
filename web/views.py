e992d3287ffc71cfa55b384a7e6fe896e9d1db73
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:35:18 +0300

    + filter with AND

--- web/views.py
+++ web/views.py
@@ -13,7 +13,10 @@ def main_view(request):
         notes = notes.filter(alert_send_at__isnull=False)
 
     if search:
-        notes = notes.filter(title_icontains=search)
+        notes = notes.filter(
+            title__icontains=search,
+            text__icontains=search
+        )
 
     return render(request, "web/main.html", {
         'count': Note.objects.count(),
