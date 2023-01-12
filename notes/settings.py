4004f937f97185a7c594dfea01563163acd78cc2
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:06:05 +0300

    + postgres

--- notes/settings.py
+++ notes/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Django_lesson_project_3sem',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
