60c55f3ad555a2804e65a8157ac28119ac4da605
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-12 23:50:43 +0300

    + base model

--- web/models.py
+++ web/models.py
@@ -4,16 +4,23 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class Note(models.Model):
+
+class BaseModel(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+
+class Note(BaseModel):
 	title = models.CharField(max_length=500)
 	text = models.TextField()
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	alert_send_at = models.DateTimeField(null=True)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
 
 
-class Tag(models.Model):
+class Tag(BaseModel):
 	title = models.CharField(max_length=200)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	parent_tag = models.ForeignKey(
@@ -21,5 +28,3 @@ class Tag(models.Model):
 		on_delete=models.SET_NULL,
 		null=True
 	)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
