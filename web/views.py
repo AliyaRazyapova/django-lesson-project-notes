8adea5d956b76404073c9f222e89feac8258c29b
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:22:59 +0300

    + with_alerts get param

--- web/views.py
+++ web/views.py
@@ -5,9 +5,11 @@ from web.models import Note
 
 
 def main_view(request):
+    with_alerts = 'with_alerts' in request.GET
     notes = Note.objects.all()
 
     return render(request, "web/main.html", {
         'count': Note.objects.count(),
-        'notes': notes
+        'notes': notes,
+        'with_alerts': with_alerts
     })
