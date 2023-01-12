34527ac0e2e1ed1aab75b49cd8b4188c4acaf043
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:09:51 +0300

    + first view

--- web/urls.py
+++ web/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from web.views import main_view
 
 urlpatterns = [
-    path('', include('web.urls')),
-    path('admin/', admin.site.urls),
+    path("", main_view),
 ]
