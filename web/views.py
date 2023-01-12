3b2294a8b44096bdf1a766513ad7d7d5fa1d8c3a
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:23:22 +0300

    + alerts filter

--- web/views.py
+++ web/views.py
@@ -8,6 +8,9 @@ def main_view(request):
     with_alerts = 'with_alerts' in request.GET
     notes = Note.objects.all()
 
+    if with_alerts:
+        notes = notes.filter(alert_send_at__isnull=False)
+
     return render(request, "web/main.html", {
         'count': Note.objects.count(),
         'notes': notes,
