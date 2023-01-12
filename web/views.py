2580fa2a78515d839c4dc2ed7b63517ef94a04ec
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:12:55 +0300

    + print info from models

--- web/views.py
+++ web/views.py
@@ -1,6 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from web.models import Note
+
 
 def main_view(request):
+    notes = Note.objects.all()
+    for note in notes:
+        print(note.title)
+        print(note.text)
+        print(note.user)
+        print(note.user.first_name)
     return render(request, 'web/main.html')
