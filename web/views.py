561c67e173bca9ff00617c1af1f6e895744a7f5e
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:28:28 +0300

    + filter exact

--- web/views.py
+++ web/views.py
@@ -6,14 +6,20 @@ from web.models import Note
 
 def main_view(request):
     with_alerts = 'with_alerts' in request.GET
+    search = request.GET.get('search', None)
     notes = Note.objects.all()
 
     if with_alerts:
         notes = notes.filter(alert_send_at__isnull=False)
 
+    if search:
+        # SELECT * FROM web_note WHERE title = "..."
+        notes = notes.filter(title=search)
+
     return render(request, "web/main.html", {
         'count': Note.objects.count(),
         'notes': notes,
         'with_alerts': with_alerts,
-        'query_params': request.GET
+        'query_params': request.GET,
+        'search': search
     })
