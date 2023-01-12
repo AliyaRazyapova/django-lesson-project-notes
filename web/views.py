56127a8237303908c04ec1ec7d1fb79501f86868
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 00:16:25 +0300

    + get related entity id

--- web/views.py
+++ web/views.py
@@ -6,9 +6,19 @@ from web.models import Note
 
 def main_view(request):
     notes = Note.objects.all()
+
     for note in notes:
         print(note.title)
         print(note.text)
         print(note.user)
         print(note.user.first_name)
+
+        # SELECT auth_user.id FROM web_note
+        # INNER JOIN auth_user ON web_note.user_id = auth_user.id
+        print(note.user.id)
+
+        # SELECT user_id FROM web_note
+        print(note.user_id)
+
+
     return render(request, 'web/main.html')
