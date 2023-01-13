05474d99a3b0a9738b4a2fcbd70b63604d3e46c8
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 08:16:00 +0300

    + test notes list

--- web/tests.py
+++ web/tests.py
@@ -1,15 +1,16 @@
+from http import HTTPStatus
+
 from django.test import TestCase
+from django.urls import reverse
 
 from web.models import Note, User
 
 
-class ExampleTestCase(TestCase):
-    def test_example(self):
-        note = Note.objects.create(
-            title='test',
-            text='test',
-            user=User.objects.create(
-                email='test@test.ru'
-            )
-        )
-        self.assertIsNotNone(note.created_at)
+class NoteFiltersTestCase(TestCase):
+    def test_list(self):
+        user = User.objects.create(email='test@test.ru')
+        note = Note.objects.create(title='test note title', text='test', user=user)
+        self.client.force_login(user)
+        response = self.client.get(reverse('notes_list'))
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, note.title)
