861224aaf36b4098c37c1204337895d037993235
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 06:12:09 +0300

    + admin - list enhancements

--- web/admin.py
+++ web/admin.py
@@ -6,6 +6,9 @@ from web.models import Note
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'user', 'created_at')
     list_display_links = ('id', 'title')
+    search_fields = ('title', 'text')
+    list_filter = ('created_at',)
+    ordering = ('created_at',)
 
 
 admin.site.register(Note, NoteAdmin)
