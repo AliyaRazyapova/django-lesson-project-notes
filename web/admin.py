f676ecf58a77b3fab7af3466ad80c5c75ff71bae
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 06:18:23 +0300

    + tag admin enhancements

--- web/admin.py
+++ web/admin.py
@@ -32,5 +32,20 @@ class NoteAdmin(admin.ModelAdmin):
         return len(instance.text)
 
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'user')
+
+    def has_delete_permission(self, request, obj=None):
+        if obj is None:
+            return False
+        return request.user == obj.user
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
 admin.site.register(Note, NoteAdmin)
-admin.site.register(Tag)
+admin.site.register(Tag, TagAdmin)
