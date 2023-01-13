afaad9791a3f1ae3dd79e467c435c20434529913
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 08:14:32 +0300

    + simple test

--- web/tests.py
+++ web/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from web.models import Note, User
+
+
+class ExampleTestCase(TestCase):
+    def test_example(self):
+        note = Note.objects.create(
+            title='test',
+            text='test',
+            user=User.objects.create(
+                email='test@test.ru'
+            )
+        )
+        self.assertIsNotNone(note.created_at)
