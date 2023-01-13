e0e925337f0713c80753046dd7dfc9549dbd4062
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 14:27:41 +0300

    + nested serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,9 +1,24 @@
 from rest_framework import serializers
 
-from web.models import Note
+from web.models import Note, User, NoteComment
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'email')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NoteComment
+        fields = ('id', 'text')
 
 
 class NoteSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    comments = CommentSerializer(many=True)
+
     class Meta:
         model = Note
-        fields = ('id', 'title', 'created_at')
+        fields = ('id', 'title', 'user', 'comments', 'created_at')
