65929d35b378c9fa3b7e45fd13673cd2d4a03af2
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 06:13:01 +0300

    + admin - custom field

--- web/admin.py
+++ web/admin.py
@@ -4,11 +4,18 @@ from web.models import Note
 
 
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'user', 'created_at')
+    list_display = ('id', 'title', 'user', 'get_text_count', 'created_at')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'text')
     list_filter = ('created_at',)
     ordering = ('created_at',)
+    readonly_fields = ('alert_send_at', 'get_text_count')
+
+    # exclude = ('tags',)
+
+    @admin.display(description='Text count')
+    def get_text_count(self, instance):
+        return len(instance.text)
 
 
 admin.site.register(Note, NoteAdmin)
