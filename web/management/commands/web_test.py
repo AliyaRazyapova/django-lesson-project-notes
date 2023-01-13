ae192375b0b668ef8f7514de76bf6c2ec858eae9
Razyapova Aliya <a.raziapova@mail.ru>
2023-01-13 06:58:23 +0300

    + get or create

--- web/management/commands/web_test.py
+++ web/management/commands/web_test.py
@@ -1,6 +1,13 @@
 from django.core.management.base import BaseCommand
 
+from web.enums import Role
+from web.models import Note, User
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print("hello world")
+        user, is_created = User.objects.get_or_create(
+            email='user@test.com',
+            defaults={'role': Role.staff}
+        )
+        print(user, is_created)
